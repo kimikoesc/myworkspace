3e5b87564af65039c6abe6356edc9fb80e7d47ed
Kimiko <kimikoescovilla@gmail.com>
2021-11-03 12:05:55 +0900

    feat: added the database.

--- base/admin.py
+++ base/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Workspace, WorkspaceUser, Tasks
 
 # Register your models here.
+admin.site.register(Workspace)
+admin.site.register(WorkspaceUser)
+admin.site.register(Tasks)
