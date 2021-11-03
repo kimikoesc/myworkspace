2b37d7c72146420ef5e00f282146e10491c9cc67
Kimiko <kimikoescovilla@gmail.com>
2021-11-03 16:37:00 +0900

    feat: added CRUD to workspace and tasks.

--- base/urls.py
+++ base/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
-from .views import WorkspaceList, WorkspaceTasks
+from .views import WorkspaceList, WorkspaceTasks, WorkspaceCreate, WorkspaceUpdate, WorkspaceDelete, TaskCreate, TaskUpdate, TaskDelete
  
 urlpatterns = [
     path('', WorkspaceList.as_view(), name="workspacelist"),
-    path('workspace/', WorkspaceTasks.as_view(), name="view-workspace")
+    path('workspace/', WorkspaceTasks.as_view(), name="view-workspace"),
+    path('workspace/new', WorkspaceCreate.as_view(), name="new-workspace"),
+    path('workspace/<int:pk>/edit', WorkspaceUpdate.as_view(), name="edit-workspace"),
+    path('workspace/<int:pk>/delete', WorkspaceDelete.as_view(), name="delete-workspace"),
+    path('tasks/new', TaskCreate.as_view(), name="new-task"),
+    path('tasks/<int:pk>/edit', TaskUpdate.as_view(), name="edit-task"),
+    path('tasks/<int:pk>/delete', TaskDelete.as_view(), name="delete-task"),
     ]  
--- base/views.py
+++ base/views.py
@@ -28,3 +28,34 @@ class WorkspaceTasks(ListView):
         if selectedWorkspace:
             context['tasks'] = context['tasks'].filter(workspace=selectedWorkspace)
             return context
+
+class WorkspaceCreate(CreateView):
+    model = Workspace
+    fields = "__all__"
+    success_url = reverse_lazy('workspacelist')
+
+class WorkspaceUpdate(UpdateView):
+    model = Workspace
+    fields = "__all__"
+    success_url = reverse_lazy('workspacelist')
+
+class WorkspaceDelete(DeleteView):
+    model = Workspace
+    context_object_name = 'workspace'
+    success_url = reverse_lazy('workspacelist') 
+
+
+class TaskCreate(CreateView):
+    model = Tasks
+    fields = "__all__"
+    success_url = reverse_lazy('view-workspace')
+
+class TaskUpdate(UpdateView):
+    model = Tasks
+    fields = "__all__"
+    success_url = reverse_lazy('view-workspace')
+
+class TaskDelete(DeleteView):
+    model = Tasks
+    context_object_name = 'task'
+    success_url = reverse_lazy('view-workspace')
