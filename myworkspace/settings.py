1f529c611f1f4e98643cf7cab0bf4b8bf4b79c9a
Kimiko <kimikoescovilla@gmail.com>
2021-11-05 02:10:14 +0900

    added more css.

--- myworkspace/settings.py
+++ myworkspace/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-15l*2f3mdm3k(_-5x&^tpj-z)=q=&$5=m*q_d$a8zn$zb6ro$p
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['myworkspace-app.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
