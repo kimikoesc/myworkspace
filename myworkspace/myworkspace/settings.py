fa13fca480d60b4f33b69138cac20f848887d2a8
Kimiko <kimikoescovilla@gmail.com>
2021-12-09 21:03:19 +0900

    feat: fixed login button.

--- myworkspace/myworkspace/settings.py
+++ myworkspace/myworkspace/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-15l*2f3mdm3k(_-5x&^tpj-z)=q=&$5=m*q_d$a8zn$zb6ro$p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['myworkspace-app.herokuapp.com', '127.0.0.1']
 
